bc1c5b3a28d2e6d371c477b73e550fc4494f3547
tidto <itnttoremayrain@gmail.com>
2026-04-09 11:15:09 +0900

    JUnit 테스트 실습 코드 추가

--- src/test/java/JUnitCycleTest.java
+++ src/test/java/JUnitCycleTest.java
@@ -1,42 +1,32 @@
 import org.junit.jupiter.api.*;
 
 public class JUnitCycleTest {
-
-    @BeforeEach // 테스트 케이스 실행하기 이전에 실행
-    public void prepareEachTest(){
-        System.out.println("prepareEachTest");
-
+    @BeforeEach // 각 테스트 케이스를 시작하기 전에 실행
+    public void prepareEachTest() {
+        System.out.println("각 테스트 전 준비 실행");
     }
-
-    @AfterEach // 각 테스트 케이스 실행한 이후 실행
-    public void afterEachTest(){
-        System.out.println("afterEachTest");
+    @AfterEach // 각 테스트 케이스를 실행한 후에 실행
+    public void cleanEachTest() {
+        System.out.println("각 테스트 후 설겆이 실행");
     }
-
-    @BeforeAll // 모든 테스트 실행전 static 메소드로 만들어야함
-    static void PrepareAllTest(){
-        System.out.println("PrepareAllTest");
-    }
-
-    @AfterAll // 모든 테스트 수행후
-    static void CleanAllTest(){
-        System.out.println("CleanAllTest");
-    }
-
-
     @Test
-    public void test1(){
-
+    public void test1() {
         System.out.println("test1");
     }
-
     @Test
-    public void test2(){
+    public void test2() {
         System.out.println("test2");
     }
-
     @Test
-    public void test3(){
+    public void test3() {
         System.out.println("test3");
+    }
+    @BeforeAll
+    static void prepareTotal() {
+        System.out.println("모든 테스트 수행 전 준비작업");
+    }
+    @AfterAll
+    static void cleanTotal() {
+        System.out.println("모든 테스트 수행 후 마지막 설겆이 작업");
     }
 }
--- src/test/java/JUnitQuiz.java
+++ src/test/java/JUnitQuiz.java
@@ -4,23 +4,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JUnitQuiz {
 
     @Test
-    public void JunitQuizTest(){
-        String name1 = "Hong";
-        String name2 = "Hong";
-        String name3 = "dong";
+    public void junitQuiz1() {
+        String name1 = "홍길동";
+        String name2 = "홍길동";
+        String name3 = "홍길은";
 
-        //모든 변수가 NotNull인지 확인
+        // 모든 변수가 null이 아닌지 확인
         assertThat(name1).isNotNull();
         assertThat(name2).isNotNull();
         assertThat(name3).isNotNull();
 
-        assertThat(name1).isNotEqualTo(name2);
+        // name1과 name2가 같은지 확인
+        assertThat(name1).isEqualTo(name2);
 
-        int num1 = 1;
-        int num2 = 2;
-        int num3 = 3;
-        assertThat(num1).isGreaterThan(num2);
-        assertThat(num1).isLessThan(num2);
+        // name1과 name3이 다른지 확인
+        assertThat(name1).isNotEqualTo(name3);
     }
 
+    @Test
+    public void junitQuiz2() {
+        int number1 = 15;
+        int number2 = 0;
+        int number3 = -5;
+
+        // number1은 양수인지 확인
+        assertThat(number1).isPositive();
+
+        // number2은 0인지 확인
+        assertThat(number2).isZero();
+
+        // number3은 음수인지 확인
+        assertThat(number3).isNegative();
+
+        // number1은 number2보다 큰지 확인
+        assertThat(number1).isGreaterThan(number2);
+
+        // number3은 number2보다 작은지 확인
+        assertThat(number3).isLessThan(number2);
+    }
 }
